05e1bc24fdc187f3290d85c75d315d38beb25253
Gabbyskii <gkadiga1@gmail.com>
2026-04-19 17:51:57 +0200

    nogle klasser setup med constructors - uncomplete

--- src/Category.java
+++ src/Category.java
@@ -1,4 +1,22 @@
 public enum Category {
-
+    CRIME,
+    WAR,
+    DRAMA,
+    FAMILY,
+    ROMANCE,
+    SCI_FI,
+    FANTASY,
+    MYSTERY,
+    THRILLER,
+    ADVENTURE,
+    HISTORY,
+    BIOGRAPHY,
+    HORROR,
+    ANIMATION,
+    ACTION,
+    WESTERN,
+    DOCUMENTARY,
+    TALK_SHOW,
+    SPORT
 
 }
