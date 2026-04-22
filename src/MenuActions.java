1fa79ebbda114999adebfe6e84d9c6adcf19338c
rean0002 <rean0002@stud.kea.dk>
2026-04-22 10:51:53 +0200

    rettelser

--- src/MenuActions.java
+++ src/MenuActions.java
@@ -43,7 +43,7 @@ public class MenuActions {
     public void showWatchedMedia() {
         List<Media> watchedMedia = currentUser.getWatchedMedia();
 
-        ui.displayMessage("Dine gemte medier: ");
+        ui.displayMessage("Dine sete medier:");
         for (Media m : watchedMedia) {
             ui.displayMessage("-" + m.getTitle());
         }
@@ -62,7 +62,7 @@ public class MenuActions {
     public void showAllCategories() {
         int index = 1;
         for (Category c : Category.values()) {
-            System.out.println(index + "- " + c);
+            ui.displayMessage(index + "- " + c);
             index++;
         }
     }
@@ -82,7 +82,7 @@ public class MenuActions {
     public void showMediaByCategory(Category category) {
         for (Media m : medialist) {
             if (m.getCategories().contains(category)) {
-                System.out.println(m.getTitle());
+                ui.displayMessage(m.getTitle());
             }
         }
     }
